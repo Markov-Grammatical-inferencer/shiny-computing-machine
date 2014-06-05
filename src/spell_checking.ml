9a330993055b241015b873dec8e6ff7fafd1586a
jaked122 <jaked122@gmail.com>
2014-06-05 11:43:44 -0400

    Fixed a great deal of errors in the spelling checker, added serialization functionality.
    Changed the aspell-train so that it learns from existing english (Lovecraft is
    sadly not a good candidate for average English).
    Added spelling tests to tests.ml, so far it seems to work, mostly.

--- src/spell_checking.ml
+++ src/spell_checking.ml
@@ -11,27 +11,38 @@ let load_test_file name=
   let red_file=(fun fil->
     try
       while true do
-	j:=[input_line fil]@(!j);
+	j:=[String.lowercase (input_line fil)]@(!j);
       done;
     with End_of_file->
       ()) in 
   let get_entries=(fun x->
-		   Str.string_match (Str.regexp "\\([a-z]+\\)    \\([a-z]+\\)") x 0;
-		   (Str.matched_group 1 x),(Str.matched_group 2 x)) in 
+		   if Str.string_match (Str.regexp "[a-z]+") x 0 then
+		     Str.matched_string x
+		   else ""		  ) in 
   red_file i;
   List.map get_entries !j;;
   
+
 let nwords: Stringset.t ref=ref Stringset.empty;;
-let modelling:(string,int)Hashtbl.t =Hashtbl.create 1;;
 let train features=
 	  let models=Hashtbl.create 2 in 
 	  List.iter (
-	      fun x-> 
+	      fun x->
 	      match x with
-	      | n when Hashtbl.mem models n->Hashtbl.replace models x 1
+	      | n when not (Hashtbl.mem models n)->Hashtbl.replace models x 1;
+					     nwords:=Stringset.add x !nwords
 	      | n ->let i=Hashtbl.find models n in 
 		    Hashtbl.replace models n (i+1)) features;
 	  models;;
+
+let modelling:(string,int)Hashtbl.t =Hashtbl.create 10;;
+
+let load_and_train file=
+  let i=load_test_file file in 
+  let j=train i in 
+  Hashtbl.iter (fun x y->
+		Hashtbl.replace modelling x y) j;;
+
 let alphabet="abcdefghijklmnopqrstuvwxyz";;
 let deletes (things:(string*string) list)=
   List.map (fun (c:string *string)->
@@ -64,7 +75,7 @@ let replaces (things:(string * string) list)=
 
 let edits word=
   let s=ref[] in 
-  for i=0 to ((String.length word)+1) do
+  for i=0 to ((String.length word)-1) do
     s:=[(String.sub word 0 i),(String.sub word i ((String.length word)-i))]@(!s);
   done;
   let i=(replaces !s)@(inserts !s)@(transposes !s)@(deletes !s) in 
@@ -94,17 +105,54 @@ let known words=
 	       i:=Stringset.add x !i) words;
   !i;;
 
+let serialize file=
+  (*Serialize the dictionary, so that it may be used more quickly in the future*)
+  let out=open_out file in 
+  Hashtbl.iter (fun x y->
+		Printf.fprintf out "%s %d\n" x y) modelling;
+  print_endline "Succeeded in serializing output";
+  close_out out;;
+
+let deserialize file=
+  (*unserialize a dictionary so that it may be used immediately*)
+  let i=open_in file in 
+  let lines=ref[] in 
+  let get_contents=
+    (fun ()->
+    try
+      while true do
+	lines:=[input_line i]@(!lines);
+      done;
+    with End_of_file->close_in i) in 
+  get_contents ();
+  let extract_info=(fun x->
+		     Str.string_match (Str.regexp "\\([a-z]+\\) \\([0-9]+\\)") x 0;
+		     (Str.matched_group 1 x),(int_of_string (Str.matched_group 2 x))) in 
+  let values=List.map extract_info !lines in 
+  List.iter (fun c->
+	     let x,y=c in 
+	     nwords:=Stringset.add x !nwords;
+	     Hashtbl.add modelling x y) values;;
+
 let correct word=
-  let a=(Stringset.union (known (Stringset.elements (edits word))) (known [word])) in 
+  let a=(Stringset.union (known (Stringset.elements (edits word))) (known_edits word)) in 
   let b=(Stringset.union (known_edits word) a) in 
-  let c=Stringset.add word b in 
+  let c=Stringset.remove "" (Stringset.add word b) in 
   let mx=ref 0 in 
   let candid=ref "" in
-  Stringset.iter (fun x->let j=Hashtbl.find modelling x in 
-		    if j>(!mx) then 
-		      begin 
-			mx:=j;
-			candid:=x;
-		      end;) c;
+  print_endline (string_of_int !mx);
+  Stringset.iter (fun x->
+		  if Hashtbl.mem modelling x then 
+		    begin
+		      print_endline "checking inner thing";
+		      let j=Hashtbl.find modelling x in 
+		      if j>(!mx) then 
+			begin 
+			  print_endline (string_of_int j);
+			  print_endline x;
+			  mx:=j;
+			  candid:=x;
+			end
+		    end) c;
   !candid;;
   
