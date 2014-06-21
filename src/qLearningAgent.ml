cbbb3de887fb09ffb542fa5fa3e8e2b3073de052
Hayleigh Sanders <sandeh2@rpi.edu>
2014-06-21 12:55:14 -0400

    Continued progress on python -> ocaml conversion,  more functionality added

--- src/qLearningAgent.ml
+++ src/qLearningAgent.ml
@@ -1,18 +1,23 @@
 from game import *
 from learningAgents import ReinforcementAgent
 from featureExtractors import *
-
 import random,util,math
 
-class QLearningAgent(ReinforcementAgent):
-    def __init__(self, **args):
+module type TemplateParameters =
+sig
+    type ReinforcementAgent
+end;;
+
+module  QLearningAgent = functor (T : TemplateParameters) ->
+struct
+    let __init__(self, **args):
         ReinforcementAgent.__init__(self, **args)
         self.qStates = util.Counter()
 
-    def getQValue(self, state, action):
+    let getQValue(self, state, action):
         return self.qStates[(state,action)]
 
-    def computeValueFromQValues(self, state):
+    let computeValueFromQValues(self, state):
         topQval = float('-inf')
         for action in self.getLegalActions(state):
           if self.getQValue(state,action) > topQval:
@@ -21,7 +26,7 @@ class QLearningAgent(ReinforcementAgent):
           return 0.0
         return topQval
 
-    def computeActionFromQValues(self, state):
+    let computeActionFromQValues(self, state):
         bestActions = []
         bestValue = float('-inf')
         for action in self.getLegalActions(state):
@@ -34,13 +39,13 @@ class QLearningAgent(ReinforcementAgent):
           return random.choice(bestActions) 
         return None
     
-    def getValue(self, state):
+    let getValue(self, state):
         return self.computeValueFromQValues(state)
 
-    def getPolicy(self, state):
+    let getPolicy(self, state):
         return self.computeActionFromQValues(state)
 
-    def getAction(self, state):
+    let getAction(self, state):
         legalActions = self.getLegalActions(state)
         action = None
         if util.flipCoin(self.epsilon):
@@ -49,12 +54,19 @@ class QLearningAgent(ReinforcementAgent):
           return self.computeActionFromQValues(state)
         return action
 
-    def update(self, state, action, nextState, reward):
+    let update(self, state, action, nextState, reward):
         topQval = float('-inf')
         self.qStates[(state,action)] = (1-self.alpha)*self.getQValue(state,action)+self.alpha*(reward+self.discount*self.computeValueFromQValues(nextState))
+end;;
 
-class PacmanQAgent(QLearningAgent):
-       def __init__(self, epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0, **args):
+module type TemplateParameters =
+sig
+    type QLearningAgent
+end;;
+
+module PacmanQAgent = functor (QLearningAgent) ->
+struct
+    let __init__(self, epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0, **args):
         args['epsilon'] = epsilon
         args['gamma'] = gamma
         args['alpha'] = alpha
@@ -62,32 +74,39 @@ class PacmanQAgent(QLearningAgent):
         self.index = 0  # This is always Pacman
         QLearningAgent.__init__(self, **args)
 
-    def getAction(self, state):
+    let getAction(self, state):
         action = QLearningAgent.getAction(self,state)
         self.doAction(state,action)
         return action
 
-class ApproximateQAgent(PacmanQAgent):
-    def __init__(self, extractor='IdentityExtractor', **args):
+module type TemplateParameters =
+sig
+    type PacmanAgent
+end;;
+
+module ApproximateQAgent = functor (T : TemplateParameters) ->
+struct
+    let __init__(self, extractor='IdentityExtractor', **args):
         self.featExtractor = util.lookup(extractor, globals())()
         PacmanQAgent.__init__(self, **args)
         self.weights = util.Counter()
 
-    def getWeights(self):
+    let getWeights(self):
         return self.weights
 
-    def getQValue(self, state, action):
+    let getQValue(self, state, action):
         feature = self.featExtractor.getFeatures(state,action)
         return self.weights*feature
 
-    def update(self, state, action, nextState, reward):
+    let update(self, state, action, nextState, reward):
         diff = (reward+self.discount*self.computeValueFromQValues(nextState))-self.getQValue(state,action)
         feature = self.featExtractor.getFeatures(state,action)
         for f in feature.keys():
           self.weights[f] = self.getWeights()[f]+(self.alpha*diff*feature[f])
 
-    def final(self, state):
+    let final(self, state):
         PacmanQAgent.final(self, state)
         if self.episodesSoFar == self.numTraining:
             pass
 
+end;;
