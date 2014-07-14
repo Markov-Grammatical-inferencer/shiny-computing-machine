b32f0d2598ad69cdd2a3d7a0b5d568ddfac28a83
Hayleigh Sanders <sandeh2@rpi.edu>
2014-07-14 10:00:59 -0400

    Added priority queue functionality to util file.

--- src/machine_learning_util.ml
+++ src/machine_learning_util.ml
@@ -170,7 +170,35 @@ class ['a] Queue =
         "Returns true if the queue is empty"
         return len(self.list) == 0*)
 
-class PriorityQueue:
+class PriorityQueue = 
+    struct
+        type priority = int
+        type 'a queue = Empty | Node of priority * 'a * 'a queue * 'a queue
+        let empty = Empty
+        let rec insert queue prio elt = 
+            match queue with
+                Empty -> Node(prio, elt, Empty, Empty)
+            | Node(p, e, left, right) -> 
+                if prio <= p
+                then Node(prio, elt, insert right p e, left)
+                else Node(p, e, insert right prio elt, left)
+        exception Queue_is_empty
+        let rec remove_top = function
+            Empty -> raise Queue_is_empty
+            | Node(prio, elt, left, Empty) -> left
+            | Node(prio, elt, Empty, right) -> right
+            | Node(prio, elt, (Node(lprio, lelt, _, _) as left),
+                                (Node(rprio, relt, _, _) as right)) ->
+                if lprio <= rprio
+                then Node(lprio, lelt, remove_top left, right)
+                else Node(rprio, relt, left, remove_top right)
+        let extract = function
+            Empty -> raise Queue_is_empty
+            | Node(prio, elt, _, _) as queue -> (prio, elt, remove_top queue)
+    end;;
+            
+
+(*class PriorityQueue:
     """
       Implements a priority queue data structure. Each inserted item
       has a priority associated with it and the client is usually interested
@@ -199,7 +227,7 @@ class PriorityQueue:
         return item
 
     def isEmpty(self):
-        return len(self.heap) == 0
+        return len(self.heap) == 0*)
 
 class PriorityQueueWithFunction(PriorityQueue):
     """
