07d8a36f097562e6891e0a5368a8a52bab77a03d
Avi Weinstock <aweinstock314@gmail.com>
2014-07-24 16:29:36 -0400

    Cleaned up Markov_chain_generator slightly.

--- src/markov_chain_generator.ml
+++ src/markov_chain_generator.ml
@@ -8,22 +8,9 @@ open Scm_util;;
 open Markov_chain_generator;;
 *)
 
-
-let string_of_file fname =
-    let file = open_in fname in
-    let buf = Array.create (in_channel_length file) '0' in
-    Array.iteri (fun idx _ ->
-         Array.set buf idx $ input_char file) buf;
-    string_of_array buf;;
-
 let tokenize = Glr_parser.tokenize_via_whitespace;;
 
-let array_shift_left new_rightmost arr =
-    let last = (Array.length arr)-1 in
-    Array.iteri (fun idx elt ->
-        let next_val = if idx = last then new_rightmost else Array.get arr (idx+1) in
-        Array.set arr idx next_val
-    ) arr;;
+let tokens_of_file = compose tokenize string_of_file;;
 
 (* a queue that only maintains that last n items, dropping the least recent if more are appended *)
 class ['a] finite_queue length init =
@@ -31,25 +18,26 @@ object(self)
 val state : 'a array = Array.create length init
 method get = Array.copy state
 method set arr = Array.iteri (Array.set state) arr
-method push elt = array_shift_left elt state;
+method push elt = Array.shift_left elt state;
 end;;
 
-let hashtbl_inplace_key tbl k fn default = Hashtbl.replace tbl k $ fn (Hashtbl.find_default default tbl k);;
+(* window_length * (token array -> token -> occurrence count) *)
+type occurrence_data = int * (string array, (string, int) Hashtbl.t) Hashtbl.t
 
-let generate_markov_counts window tokens =
+let generate_markov_counts window tokens : occurrence_data =
     let q = new finite_queue window "" in
     let table = Hashtbl.create 0 in
     let subtable ctx =
         try Hashtbl.find table ctx with Not_found ->
         (let tmp = Hashtbl.create 0 in Hashtbl.add table ctx tmp; tmp) in
-    let do_count tbl x = hashtbl_inplace_key tbl x ((+) 1) 0 in
+    let do_count tbl x = Hashtbl.inplace_key tbl x ((+) 1) 0 in
     List.iter (fun tok ->
         do_count (subtable q#get) tok;
         q#push tok;
     ) tokens;
-    table;;
+    (window, table);;
 
-let generate_randomish_string window markov_table length =
+let generate_randomish_string ((window, markov_table) : occurrence_data) length =
     let ctx = new finite_queue window "" in
     let random_word c =
         let subtbl = Hashtbl.find_default (Hashtbl.create 0) markov_table c in
@@ -68,5 +56,5 @@ let generate_randomish_string window markov_table length =
 
 let main filename window length =
     let tokens = tokenize $ string_of_file filename in
-    let tbl = generate_markov_counts window tokens in
-    generate_randomish_string window tbl length;;
+    let occurrences = generate_markov_counts window tokens in
+    generate_randomish_string occurrences length;;
